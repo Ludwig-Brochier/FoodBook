d53f47fb46b525d9a27df9c2c541f1cbbe3e91db
Ludwig-Brochier <ludwig.brochier@gmail.com>
2021-06-10 11:22:40 +0200

    Lancement Controllers

---
+++ Server/API/Controllers/MenusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public class MenusController : ControllerBase
+    {
+
+    }
+}
