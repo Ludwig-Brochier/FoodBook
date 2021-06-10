ab34f1c9a797a07b4b6c95c109ec4f2363729baa
Ludwig-Brochier <ludwig.brochier@gmail.com>
2021-06-10 15:37:05 +0200

    Début BLL

---
+++ Server/BLL/Services/ICommandeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO.DTO.Reponses;
+using BO.Entite;
+
+namespace BLL.Services
+{
+    public interface ICommandeService
+    {
+        Task<ReponsePagination<Ingredient>> CommandeIngredientsAsync(int idSemaine);
+    }
+}
---
+++ Server/BLL/Services/IManagementService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO.Entite;
+
+namespace BLL.Services
+{
+    public interface IManagementService
+    {
+        /// <summary>
+        /// Permet de récupérer une semaine précise en fonction de son identifiant
+        /// </summary>
+        /// <param name="idSemaine">Idenfiant de la semaine</param>
+        /// <returns>L'objet Semaine demandé</returns>
+        Task<Semaine> GetSemaineAsync(int idSemaine);
+
+        /// <summary>
+        /// Permet de créer une nouvelle semaine
+        /// </summary>
+        /// <param name="semaine">La semaine à créer</param>
+        /// <returns>L'objet Semaine créé</returns>
+        Task<Semaine> InsertSemaineAsync(Semaine semaine);
+
+        /// <summary>
+        /// Permet de mettre à jour une semaine éxistante
+        /// </summary>
+        /// <param name="semaine">Le nouvel objet Semaine</param>
+        /// <returns>L'objet Semaine mis à jour</returns>
+        Task<Semaine> UpdateSemaineAsync(Semaine semaine);
+
+        /// <summary>
+        /// Permet de supprimer une semaine en fonction de son identifiant
+        /// </summary>
+        /// <param name="idSemaine">L'identifiant de la semaine</param>
+        /// <returns bool>Si l'objet Semaine est supprimé</returns>
+        Task<bool> DeleteSemaineAsync(int idSemaine);
+    }
+}
