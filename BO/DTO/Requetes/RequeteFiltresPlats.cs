eb3fdcfa14cbaf80bd5e5bcfe0421fd1e0bd9c71
Ludwig-Brochier <ludwig.brochier@gmail.com>
2021-07-14 18:38:40 +0200

    refonte DTO filtre plats

---
+++ BO/DTO/Requetes/RequeteFiltresPlats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BO.DTO.Requetes
+{
+    /// <summary>
+    /// DTO permettant de filtrer des plats
+    /// Pagination possible
+    /// </summary>
+    public class RequeteFiltresPlats : RequetePagination
+    {
+        public bool Populaire { get; set; }
+
+        public string Type { get; set; }
+
+        public int IdIngredient { get; set; }
+
+
+        public RequeteFiltresPlats() : base() { }
+
+        public RequeteFiltresPlats(bool populaire, string type, int idIngredient, int page, int taillePage) : base(page, taillePage)
+        {
+            Populaire = populaire;
+            Type = type;
+            IdIngredient = idIngredient;
+        }
+    }
+}
